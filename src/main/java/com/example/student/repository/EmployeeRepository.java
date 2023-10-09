a1565801752103af53e987d3d306790935a95d78
pavan ganguru <pavanganguru999@gmail.com>
2023-10-09 16:29:55 -0400

    added employee repository

---
+++ src/main/java/com/example/student/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.example.student.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.student.entity.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
